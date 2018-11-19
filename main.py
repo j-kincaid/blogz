0bceb98bf3c19ff998dcd05014fdd6d5ad3c26a7
Jessica Kincaid <4j.kincaid@gmail.com>
2018-11-19 15:03:03 -0600

    added email verification

--- main.py
+++ main.py
@@ -28,7 +28,6 @@ class Blog(db.Model): # Create an instance of the Blog class
         self.body = body
         self.owner = owner 
 
-
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(30))
@@ -55,7 +54,7 @@ def index():
     # return render_template('singleUser.html')
 
 
-@app.route('/newpost', methods=['POST','GET']) # Submit your blogs through '/newpost' 
+@app.route('/newpost', methods=['POST','GET']) # Submit blogs through '/newpost' 
 # After you submit, the main page is displayed.
 def process_add_entry():
     title_error = ''
@@ -80,28 +79,54 @@ def process_add_entry():
     return render_template('newpost.html', title=title, body=body, title_error=title_error, body_error=body_error)
     
 
+@app.route('/signup', methods=['POST'])
+def signup():
+    username = request.form['username']
+    password = request.form['password']
+    v_password = request.form['v_password']
+    email = request.form['email']
+    
+    username_error = ''
+    password_error = ''
+    v_password_error = ''
+    email_error = ''
 
-    def signup():
-        if request.method == 'POST': # Create a new user, looking at signup.html
-            email = request.form['email']
-            password = request.form['password']
-            verify = request.form['verify']
+    ####### testing the try/except block ############
 
-        # TODO - validate user's data
+# # TODO:
+# # Check for errors and reject and render feedback:
 
-        existing_user = User.query.filter_by(email=email).first()
-        if not existing_user:
-            new_user = User(email, password)
-            db.session.add(new_user)
-            db.session.commit()
-            # TODO - remember the user
-            session['email'] = email
-            return redirect('/')
-        else:
-            # TODO - more specific response message
-            return "<h1>Duplicate User</h1>"
+    if " " in username or len(username) < 3 or len(username) > 20 or "":
+        username_error = "Username cannot have spaces and must be between 3 and 20 characters"
+ 
+   # return render_template("index.html", username=username, username_error=username_error)
 
-    return render_template('signup.html')
+    
+    if len(password) > 20 or len(password) < 3 or "" or " " in password:
+        password_error = 'Password must be between 3 and 20 characters.'
+    
+    if password != v_password:
+        # password must match verified password
+        v_password_error = "Passwords must match."
+# email must have:
+    if email:
+        if not ('@'in email):
+            email_error = "Email must contain @."
+
+        if len(email) > 20 or len(email) < 3:
+            email_error = "Email must be between 3 and 20 characters."
+
+        if not ('.' in email):
+            email_error = "Email must contain a . ."
+
+        if ' ' in email:
+            email_error = "Email must not contain spaces."
+    
+    if not username_error and not password_error and not v_password_error and not email_error:
+        return render_template("blog.html", username=username, email=email)
+    else:
+        # redisplay the form with the error messages.
+        return render_template("signup.html", username=username, username_error=username_error, password_error=password_error, v_password_error=v_password_error, email_error=email_error)
 
 if __name__ == '__main__':
 
