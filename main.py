d2f5c73d2ad27fad6cb28e1abd2668fd00101889
Jessica Kincaid <4j.kincaid@gmail.com>
2018-11-19 20:23:45 -0600

    added signup form, several @app.route decorators

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 
 # main.py
 
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, session
 from flask_sqlalchemy import SQLAlchemy
 from flask import url_for
 
@@ -13,6 +13,9 @@ app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:greenenchiladas@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
+# Add a session object and 
+app.secret_key = 'LD@R&tEX55gl'
+
 
 ############# Storing passwords in the db is not a good idea, so eventually I will modify it accordingly.
 
@@ -32,13 +35,11 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(30))
     password = db.Column(db.String(30))
-    user_id = db.Column(db.Integer, db.ForeignKey('blog.id'))
-    # blogs = `db.relationship('Blog', backref='user')`
+    blogs = db.relationship('Blog', backref='owner')
 
     def __init__(self, username, password):
         self.username = username
         self.password = password
-
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
@@ -54,18 +55,23 @@ def index():
     # return render_template('singleUser.html')
 
 
-@app.route('/newpost', methods=['POST','GET']) # Submit blogs through '/newpost' 
+
+@app.route('/newpost', methods=['POST','GET']) 
+# Submit blogs through '/newpost' 
 # After you submit, the main page is displayed.
 def process_add_entry():
     title_error = ''
     body_error = ''
     title = ''
     body = ''
+
+    owner = User.query.filter_by(username=session['username']).first()
+
     if request.method == 'POST': # Create a new post
         
         title = request.form['title']
         body = request.form['body']
-        new_blog = Blog(title, body)
+        new_blog = Blog(title, body, owner)
         db.session.add(new_blog)
         
         if not title:
@@ -79,54 +85,96 @@ def process_add_entry():
     return render_template('newpost.html', title=title, body=body, title_error=title_error, body_error=body_error)
     
 
-@app.route('/signup', methods=['POST'])
+@app.route('/signup', methods=['GET','POST'])
 def signup():
-    username = request.form['username']
-    password = request.form['password']
-    v_password = request.form['v_password']
-    email = request.form['email']
-    
+    username = ''
     username_error = ''
     password_error = ''
     v_password_error = ''
-    email_error = ''
-
-    ####### testing the try/except block ############
-
-# # TODO:
-# # Check for errors and reject and render feedback:
-
-    if " " in username or len(username) < 3 or len(username) > 20 or "":
-        username_error = "Username cannot have spaces and must be between 3 and 20 characters"
- 
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        v_password = request.form['v_password']
+        
+        if " " in username or len(username) < 3 or len(username) > 20 or "":
+            username_error = "Username cannot have spaces and must be between 3 and 20 characters"
+    
    # return render_template("index.html", username=username, username_error=username_error)
 
     
-    if len(password) > 20 or len(password) < 3 or "" or " " in password:
-        password_error = 'Password must be between 3 and 20 characters.'
-    
-    if password != v_password:
-        # password must match verified password
-        v_password_error = "Passwords must match."
-# email must have:
-    if email:
-        if not ('@'in email):
-            email_error = "Email must contain @."
+        if len(password) > 20 or len(password) < 3 or "" or " " in password:
+            password_error = 'Password must be between 3 and 20 characters.'
+        
+        if password != v_password:
+            # password must match verified password
+            v_password_error = "Passwords must match."
 
-        if len(email) > 20 or len(email) < 3:
-            email_error = "Email must be between 3 and 20 characters."
+        existing_user = User.query.filter_by(username=username).first()
+        if existing_user:
+            username_error = "User already exists."
+        if not username_error and not password_error and not v_password_error:
+            new_user = User(username, password)
+            db.session.add(new_user)
+            db.session.commit()
+            session['username'] = username
+            return render_template("blog.html", username=username)
 
-        if not ('.' in email):
-            email_error = "Email must contain a . ."
+            # redisplay the form with the error messages.
+    return render_template("signup.html", username=username, username_error=username_error, password_error=password_error, v_password_error=v_password_error)
 
-        if ' ' in email:
-            email_error = "Email must not contain spaces."
-    
-    if not username_error and not password_error and not v_password_error and not email_error:
-        return render_template("blog.html", username=username, email=email)
-    else:
-        # redisplay the form with the error messages.
-        return render_template("signup.html", username=username, username_error=username_error, password_error=password_error, v_password_error=v_password_error, email_error=email_error)
+@app.route('/logout', methods=['GET'])
+def logout():
+    del session['username']
+    return redirect('/')
+
+@app.before_request
+def require_login():
+    allowed_routes = ['login', 'signup']
+    if request.endpoint not in allowed_routes and 'username' not in session:
+        return redirect('/login')
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    login_error = ''
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']        
+        
+        user = User.query.filter_by(username=username).first()
+        if user and user.password == password:
+            session['username'] = username
+            return redirect('/')
+        else:
+            login_error = "Username or password is incorrect"
+    return render_template('login.html', login_error=login_error)
+
+
+#`''``From get-it-done''`
+# @app.route('/', methods=['POST', 'GET'])
+# def index():
+
+# ###################_______________##################
+# #### Where the new task (or new blog post) is created 
+# ###################_______________##################
+
+
+#     owner = User.query.filter_by(email=session['email']).first()
+
+#     if request.method == 'POST':
+#         task_name = request.form['task']
+#         new_task = Task(task_name, owner) # Create a new task object
+#         db.session.add(new_task)
+#         db.session.commit() # commit it to the db
+
+#     tasks = Task.query.filter_by(completed=False, owner=owner).all() 
+#     # only give me the tasks for which the completed column has the value False
+#     completed_tasks = Task.query.filter_by(completed=True).all()
+#     return render_template('todos.html',title="Get It Done!", tasks=tasks, completed_tasks=completed_tasks)
+
+
+
+
+
 
 if __name__ == '__main__':
 
